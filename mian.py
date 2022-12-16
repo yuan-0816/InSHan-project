98bde3f5d2ce4d1a80c3e74089c5460157452df6
yuan-0816 <wj582693@gmail.com>
2022-12-17 04:23:47 +0800

    add class named mainwindow

--- mian.py
+++ mian.py
@@ -6,5 +6,9 @@ root.geometry("500x300")
 frame = ctk.CTkFrame(master=root)
 frame.pack(pady=1, padx=100, fill="x", expand=True)
 
+class main_window():
+    def __init__(self):
+        pass
+
 if __name__ == "__main__":
     root.mainloop()
