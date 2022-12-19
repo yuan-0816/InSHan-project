aeabf8444240e94595732f2da8a0bfcc0b15e128
yuan-0816 <wj582693@gmail.com>
2022-12-19 09:00:59 +0800

    20221219

--- mian.py
+++ mian.py
@@ -1,10 +1,5 @@
 import customtkinter as ctk
 
-root = ctk.CTk()
-root.geometry("500x300")
-
-frame = ctk.CTkFrame(master=root)
-frame.pack(pady=1, padx=100, fill="x", expand=True)
 
 class main_window():
     def __init__(self):
