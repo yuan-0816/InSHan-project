42677bdac614e3b4ac08dee511d5c77e3f5babfc
yuan-0816 <wj582693@gmail.com>
2022-12-19 09:28:58 +0800

    nothing

--- mian.py
+++ mian.py
@@ -1,9 +1,32 @@
-import customtkinter as ctk
+import customtkinter
+import tkinter
+import tkinter.messagebox
 
 
-class main_window():
+class main_window(customtkinter.CTk):
     def __init__(self):
-        pass
+        super().__init__()
+
+        self.title("yuan's window><")
+        self.geometry(f"{1080}x{720}")
+
+        # configure grid layout (4x4)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure((2, 3), weight=0)
+        self.grid_rowconfigure((0, 1, 2), weight=1)
+
+        # create sidebar frame with widgets
+        self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
+        self.sidebar_frame.grid_rowconfigure(4, weight=1)
+        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="CustomTkinter",
+                                                 font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
+
+
+
+
 
 if __name__ == "__main__":
-    root.mainloop()
+    app = main_window()
+    app.mainloop()
